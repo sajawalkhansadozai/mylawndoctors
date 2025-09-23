a1336affba9f823236f0bfa1e5a6b64b4aa8fad9
sajawalkhansadozai <sajawalkhansadozai74@gmail.com>
2025-09-23 17:09:19 +0500

    Include web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "358409059"
+    serviceWorkerVersion: "4260589019"
   }
 });
